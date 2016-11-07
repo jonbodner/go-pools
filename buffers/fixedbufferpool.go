ab86c162a355f0c57616ccc63e39920c33f8d09f
jdamick <jeffrey.damick@capitalone.com>
2016-11-07 13:17:25 -0500

    initial commit

---
+++ buffers/fixedbufferpool.go
@@ -0,0 +1,112 @@
+package buffers
+
+import (
+	"bytes"
+	"container/list"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+type FixedBufferPool struct {
+	bufPool  *list.List
+	lock     sync.Mutex
+	gets     uint64
+	puts     uint64
+	timeouts uint64
+	waiter   chan struct{}
+}
+
+const (
+	DefaultBufferMax   = 4096
+	DefaultBufferCount = 5
+)
+
+func NewDefaultFixedBufferPool() *FixedBufferPool {
+	return NewFixedBufferPool(DefaultBufferMax, DefaultBufferCount)
+}
+
+func NewFixedBufferPool(maxBytesPerBuffer uint64, maxBufferCount uint32) *FixedBufferPool {
+	pool := list.New()
+	for i := uint32(0); i < maxBufferCount; i++ {
+		pool.PushBack(bytes.NewBuffer(make([]byte, maxBytesPerBuffer)))
+	}
+	return &FixedBufferPool{bufPool: pool, waiter: make(chan struct{}, maxBufferCount)}
+}
+
+func (p *FixedBufferPool) Close() error {
+	close(p.waiter)
+	return nil
+}
+
+// Get selects an arbitrary buffer from the Pool
+// It may return nil if none is available.
+func (p *FixedBufferPool) Get() *bytes.Buffer {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+	el := p.bufPool.Front()
+	if el != nil {
+		p.bufPool.Remove(el)
+		atomic.AddUint64(&p.gets, 1)
+		return el.Value.(*bytes.Buffer)
+	}
+	return nil
+}
+
+func (p *FixedBufferPool) WaitForGet(maxWait time.Duration) *bytes.Buffer {
+	result := p.Get()
+
+	duration := 100 * time.Millisecond
+	if duration > maxWait {
+		duration = maxWait
+	}
+
+	if result == nil {
+		start := time.Now()
+		for result == nil {
+			select {
+			case <-p.waiter:
+				// signaled
+				result = p.Get()
+			case <-time.After(duration):
+				result = p.Get()
+			default:
+				result = p.Get()
+			}
+
+			// check max time wait
+			if result == nil && time.Since(start) > maxWait {
+				atomic.AddUint64(&p.timeouts, 1)
+				return result
+			}
+		}
+	}
+	return result
+}
+
+func (p *FixedBufferPool) Put(b *bytes.Buffer) {
+	b.Reset()
+
+	p.lock.Lock()
+	p.bufPool.PushBack(b)
+	p.lock.Unlock()
+
+	atomic.AddUint64(&p.puts, 1)
+	if len(p.waiter) != cap(p.waiter) {
+		p.waiter <- struct{}{}
+	}
+}
+
+func (p *FixedBufferPool) GetCount() uint64 {
+	return atomic.LoadUint64(&p.gets)
+}
+
+func (p *FixedBufferPool) PutCount() uint64 {
+	return atomic.LoadUint64(&p.puts)
+}
+
+func (p *FixedBufferPool) ResetCounters() {
+	atomic.StoreUint64(&p.gets, 0)
+	atomic.StoreUint64(&p.puts, 0)
+	atomic.StoreUint64(&p.timeouts, 0)
+}
---
+++ buffers/fixedbufferpool_test.go
@@ -0,0 +1,90 @@
+package buffers
+
+import (
+	"bytes"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBufferPool(t *testing.T) {
+	p := NewFixedBufferPool(5, 1)
+	assert.NotNil(t, p)
+
+	b := p.Get()
+	assert.NotNil(t, b)
+	defer p.Put(b)
+
+	b2 := p.Get()
+	assert.Nil(t, b2)
+
+	assert.True(t, b.Cap() == 5)
+
+	assert.Equal(t, uint64(1), p.GetCount())
+	assert.Equal(t, uint64(0), p.PutCount())
+
+	p.ResetCounters()
+	assert.Equal(t, uint64(0), p.GetCount())
+	assert.Equal(t, uint64(0), p.PutCount())
+}
+
+func TestBufferReset(t *testing.T) {
+	p := NewFixedBufferPool(5, 1)
+
+	b := p.Get()
+	b.WriteString("testing")
+	p.Put(b)
+
+	b2 := p.Get()
+	assert.Equal(t, 0, b2.Len())
+}
+
+func TestBufferGetWaiter(t *testing.T) {
+	p := NewFixedBufferPool(5, 1)
+	b := p.WaitForGet(20 * time.Second)
+	assert.NotNil(t, b)
+	var wg1, wg2 sync.WaitGroup
+
+	var b2 *bytes.Buffer
+	wg1.Add(1)
+	wg2.Add(1)
+	go func() {
+		wg1.Done()
+		b2 = p.WaitForGet(20 * time.Second)
+		assert.NotNil(t, b2)
+		wg2.Done()
+	}()
+
+	wg1.Wait()
+	assert.Nil(t, b2)
+	time.Sleep(1 * time.Second)
+	p.Put(b)
+	wg2.Wait()
+	assert.NotNil(t, b2)
+}
+
+func TestBufferGetWaiterTimeout(t *testing.T) {
+	p := NewFixedBufferPool(5, 1)
+	b := p.WaitForGet(10 * time.Millisecond)
+	assert.NotNil(t, b)
+	var wg1, wg2 sync.WaitGroup
+
+	var b2 *bytes.Buffer
+	wg1.Add(1)
+	wg2.Add(1)
+	go func() {
+		wg1.Done()
+		b2 = p.WaitForGet(10 * time.Millisecond)
+		assert.Nil(t, b2)
+		wg2.Done()
+	}()
+
+	wg1.Wait()
+	assert.Nil(t, b2)
+	time.Sleep(1 * time.Second)
+	p.Put(b)
+	wg2.Wait()
+	assert.Nil(t, b2)
+}
